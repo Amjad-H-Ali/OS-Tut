328c687b377bc25cc1e8a869a0f753ae6f55cd67
Amjad Ali <amjad.hamdi.ali@gmail.com>
2019-02-06 00:41:49 -0600

    Added magic number so bootloader can recognize its kernel

--- loader.s
+++ loader.s
@@ -1,3 +1,14 @@
+# Magic number so boot loader can know it's a kernel
+.set MAGIC, 0x1badb002
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+#  Put into .o file
+.section .multiboot
+	.long MAGIC
+	.long FLAGS
+	.long CHECKSUM
+
 
 .section .text
 # Telling assembler there is going to be a function called kernelMain.
@@ -5,15 +16,19 @@
 
 .global loader
 
+# Point esp register to kernel_stack
 loader:
 	mov $kernel_stack, %esp
 	call kernelMain
 
+# Backup infinit loop
 _stop:
 	cli
 	hlt
 	jmp _stop	
 
+
 .section .bss
+# Space behind kernel_stack so as to not override anything in stack already.
 .space 2*1024*1024; # 2 MB
 kernel_stack:	
