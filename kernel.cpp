1f1952f5ed9be194f7e6316faec8ec355354480c
Amjad Ali <amjad.hamdi.ali@gmail.com>
2019-02-06 02:55:14 -0600

    Created rudimentary printf function since we don't have OS to link a standard library with our program

--- kernel.cpp
+++ kernel.cpp
@@ -1,3 +1,14 @@
+// Rudimentary made printf function since we don't have access to standard library in this case.
+void printf(char* string){
+
+	unsigned short* VideoMemory =  (unsigned short*) 0xb8000;
+
+	for(int i = 0; string[i] != '\0'; i ++)
+							// To stop overriding high byte
+							// Copy High byte that was in memory.
+		VideoMemory[i] =  (VideoMemory[i] & 0xFF00) | string[i];
+};
+
 void kernelMain(void *multiboot_struct, unsigned int magic_number){
 	printf("Hello, World!\n");
 
