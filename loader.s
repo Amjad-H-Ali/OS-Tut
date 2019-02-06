4ddda7e0069635b8c00b7a4c47653e73c4d542ed
Amjad Ali <amjad.hamdi.ali@gmail.com>
2019-02-06 00:18:49 -0600

    Set esp register stack pointer to variable kernel_stack we have yet to define

--- loader.s
+++ loader.s
@@ -1,5 +1,13 @@
 
 .section .text
-
 # Telling assembler there is going to be a function called kernelMain.
 .extern kernelMain
+
+.global loader
+
+loader:
+	mov $kernel_stack, %esp
+
+.section .bss
+
+kernel_stack:	
